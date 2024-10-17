429ba52f596d83d4b219110534d4182f3e9c4e1e
Yuki Shida <shida@be-agile.jp>
2024-10-17 15:18:07 +0900

    bugfix override existsing settings of Spree::Store

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,12 +1,20 @@
 module Spree
   module StoreDecorator
     def self.prepended(base)
+      existing_settings = base.typed_stores[:settings]&.fields || {}
       base.typed_store :settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
-        s.decimal :loyalty_points_awarding_unit, default: 0.01, null: false
-        s.decimal :loyalty_points_redeeming_balance, default: 0.0, null: false
-        s.decimal :loyalty_points_conversion_rate, default: 1.0, null: false
-        s.integer :loyalty_points_award_period, default: 12, null: false
-        s.decimal :min_amount_required_to_get_loyalty_points, default: 0.0, null: false
+        existing_settings.each do |name, field|
+          s.send(field.type_sym, name, **field.instance_variables.each_with_object({}) { |var, hash|
+            hash[var.to_s.delete("@").to_sym] = field.instance_variable_get(var) unless var == :@type
+          })
+        end
+        unless existing_settings.key?(:loyalty_points_awarding_unit)
+          s.decimal :loyalty_points_awarding_unit, default: 0.01, null: false
+          s.decimal :loyalty_points_redeeming_balance, default: 0.0, null: false
+          s.decimal :loyalty_points_conversion_rate, default: 1.0, null: false
+          s.integer :loyalty_points_award_period, default: 12, null: false
+          s.decimal :min_amount_required_to_get_loyalty_points, default: 0.0, null: false
+        end
       end
     end
   end
